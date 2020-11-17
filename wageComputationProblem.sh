3f4bd16252bf2db8d66ded4888630dc68fc12c99
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-17 15:57:02 +0530

    Added:Employee Present or Absent

--- wageComputationProblem.sh
+++ wageComputationProblem.sh
@@ -2,3 +2,15 @@
 #Employee wage calculation program
 
 echo "***WELCOME TO EMPLOYEE WAGE CALCULATION PROGRAM***"
+
+isPresent=1
+randomCheck=$(($RANDOM%2));
+
+#1.employee absent or present check
+if [ $isPresent -eq $randomCheck ]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
+
