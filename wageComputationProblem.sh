b6afafeb47c820f45c73eebac6638320563dd68c
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-17 16:00:50 +0530

    Added:Calculating Daily employee wage

--- wageComputationProblem.sh
+++ wageComputationProblem.sh
@@ -14,3 +14,16 @@ else
 	echo "Employee is absent"
 fi
 
+
+#2.calculating daily employee wage
+if [ $isPresent -eq $randomCheck ]
+then
+        empRatePerHr=20
+	empHrs=8
+	salary=$(($empHrs*$empRatePerHr))
+	echo "SALARY "$salary
+else
+        salary=0
+	echo "SALARY "$salary
+fi
+
