0feaee02a5791ac9bc19e722bad7442a3548f610
vinitkulkarni3 <vinitkulkarni4321@gmail.com>
2020-11-18 08:38:28 +0530

    Added:Storing the day and daily wage

--- wageComputationProblem.sh
+++ wageComputationProblem.sh
@@ -183,3 +183,48 @@ do
 done
 totalSalary=$(($totalEmpHrs+$empRatePerHr))
 echo "Daily Wage is-> "${arrDailyWage[@]}
+
+
+
+#9.store the day and the daily wage along with total wage
+declare -A dailWageDict
+totalEmpHrs=0
+totalWorkingDays=0
+max_hrs_in_month1=4
+
+function getWorkHrs()
+{
+        empCheck2=$1
+        case $empCheck2 in
+        $isFullTime)
+                        empHrs=8;
+                        ;;
+        $isPartTime)
+                        empHrs=4;
+                        ;;
+        *)
+                        empHrs=0
+                        ;;
+        esac
+        echo $empHrs
+}
+
+function getEmpWage()
+{
+	empHr=$1
+	echo $(($empHr*$empRatePerHr))
+}
+
+while [[ $totalEmpHrs -lt $max_hrs_in_month1 &&
+         $totalWorkingDays -lt $numWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck2=$(($RANDOM%3))
+        empHrs="$( getWorkHrs $empCheck2 )"
+        totalEmpHrs=$(($totalEmpHrs+$empHrs))
+        #ans=$(($empHrs*$empRatePerHr))
+        dailyWageDict[$totalWorkingDays]="$( getEmpWage $empHrs )"
+done
+totalSalary=$(($totalEmpHrs+$empRatePerHr))
+echo "Daily Wage is dict-> "${dailyWageDict[@]}
+echo "day "${!dailyWageDict[@]}
